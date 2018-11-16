2171a770ed3a8f04059e2a66d66d0c4007953629
sagarvnarkhede <sagarvnarkhede@gmail.com>
2018-11-16 18:16:50 +0530

    fsCore lib css support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 
  import sortObjectByKey from './lib/sortObjectBykey';
  import fsChart from './chart/fsChart';
+ import './assets/css/main.scss'
 
-(function(obj){
-    obj.fsUtilLib = {
-            sortObjectByKey : sortObjectByKey,
-            fsChart:fsChart
-         };
-})(window);
+// (function(obj){
+//     obj.fsUtilLib = {
+//             sortObjectByKey : sortObjectByKey,
+//             fsChart:fsChart
+//          };
+// })(window);
 
- //module.exports = window.fsUtilLib;
+  module.exports = {
+        sortObjectByKey : sortObjectByKey,
+        fsChart:fsChart
+    };
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,75 @@
 
-
+// webpack v4
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: __dirname+'/dist',
-      filename: 'fsutil-lib.js'
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fsCore.js',
+    library: 'fsCore'
+  },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
     },
-    module: {
-      rules: [{ 
-          test: /\.js$/, 
-          exclude: /node_modules/, 
-          loader: "babel-loader" 
-        }]
+    echarts: {
+      commonjs: 'echarts',
+      commonjs2: 'echarts',
+      amd: 'echarts',
+      root: 'echarts'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader','sass-loader']
+          })
       }
-      // resolve: {
-      //   root: './src',
-      //   extensions: ['', '.js']
-      // }
-  };
+    ]
+  },
+  plugins: [ 
+    new ExtractTextPlugin({
+      filename: 'fsCore.css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  optimization: {
+    // namedModules: true, // NamedModulesPlugin()
+    // splitChunks: { // CommonsChunkPlugin()
+    //     name: 'vendor',
+    //     minChunks: 2
+    // },
+    // noEmitOnErrors: true, // NoEmitOnErrorsPlugin
+    concatenateModules: true //ModuleConcatenationPlugin
+}
+  // devServer: {
+  //   host: '0.0.0.0',
+  //   port: 8080,
+  //   contentBase: './',
+  //   hot: false,
+  //   proxy: {
+      
+  //   }
+  // }
+};;
